452815a7a37f823889b64725c39f4bcdaacb6325
Boierul <49337867+Boierul@users.noreply.github.com>
2023-09-30 13:24:45 +0200

    Chore: Implemented driving controls for PC
    Co-Authored-By: Vladimir Rotaru <31960682+senker@users.noreply.github.com>
    Co-Authored-By: Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
    Co-Authored-By: Adam Arasimowicz <75214235+arashimoya@users.noreply.github.com>

---
+++ car-viewer/Assets/Scripts/rearWheelDrive.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rearWheelDrive : MonoBehaviour
+{
+    public float maxAngle = 30;
+    public float maxTorque = 300;
+
+    public WheelCollider[] wheelColliderArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = maxAngle * Input.GetAxis("Horizontal");
+        float torque = maxTorque * Input.GetAxis("Vertical");
+
+        wheelColliderArray[0].steerAngle = angle;
+        wheelColliderArray[1].steerAngle = angle;
+
+        wheelColliderArray[2].motorTorque = torque;
+        wheelColliderArray[3].motorTorque = torque;
+
+        // Assign the each position and rotation of the wheel collider to the wheel transform 
+        foreach(WheelCollider wheel in wheelColliderArray)
+        {
+            // Get the position and rotation of the wheel collider
+            Vector3 wheelPosition;
+            Quaternion wheelRotation;
+            
+            wheel.GetWorldPose(out wheelPosition, out wheelRotation);
+
+            // Get the reference of each wheel model
+            Transform wheelModel = wheel.transform.GetChild(0);
+
+            // Assign the position of each wheel collider to the position of wheel model
+            wheelModel.position = wheelPosition;
+            // Assign the rotation of each wheel collider to the rotation of wheel model
+            wheelModel.rotation = wheelRotation;
+        }
+    }
+}
