bc8b618461ecd4f5077d3c746d5cdb5cbd1c191c
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2023-10-12 15:47:24 +0200

    fix: joystick controller
    Co-Authored-By: Dan Pintea <49337867+Boierul@users.noreply.github.com>
    Co-Authored-By: Vladimir Rotaru <31960682+senker@users.noreply.github.com>
    Co-Authored-By: Adam Arasimowicz <75214235+arashimoya@users.noreply.github.com>

--- car-viewer/Assets/Scripts/rearWheelDrive.cs
+++ car-viewer/Assets/Scripts/rearWheelDrive.cs
@@ -10,17 +10,20 @@ public class rearWheelDrive : MonoBehaviour
     public WheelCollider[] wheelColliderArray;
     public Transform[] wheelMeshArray;
 
+    //Joystick reference
+    public Joystick joystick;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float angle = maxAngle * Input.GetAxis("Horizontal");
-        float torque = maxTorque * Input.GetAxis("Vertical");
+        float angle = maxAngle * joystick.Horizontal;
+        float torque = maxTorque * joystick.Vertical;;
 
         wheelColliderArray[0].steerAngle = angle;
         wheelColliderArray[1].steerAngle = angle;
@@ -28,7 +31,7 @@ public class rearWheelDrive : MonoBehaviour
         wheelColliderArray[2].motorTorque = torque;
         wheelColliderArray[3].motorTorque = torque;
 
-        // Assign the each position and rotation of the wheel collider to the wheel transform 
+        // Assign the each position and rotation of the wheel collider to the wheel transform
         foreach (WheelCollider wheel in wheelColliderArray)
         {
             // Get the position and rotation of the wheel collider
